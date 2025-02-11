7e88bff84a9f28b5445e5c0f724662598288d1dc
Royjith <153445207+Royjith@users.noreply.github.com>
2025-02-11 15:07:36 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -5,7 +5,7 @@ from langchain_cohere import ChatCohere
 
 
 
-st.title("Cohere Chat Bot - v01")
+st.title("Cohere Chat Bot - v1")
 
 # Initialize chat history
 if "messages" not in st.session_state:
